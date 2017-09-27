3653b33bc2ca7cd44851e4022da2555797590c27
kamel-rush <kamel@grandcircus.co>
2017-09-27 10:27:17 -0400

    This is the first commit!

---
+++ HelloWorldCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input 
+            int x, y;
+
+            Console.WriteLine("Please enter the first number:");
+            x = Convert.ToInt32(Console.ReadLine());// explicit casting 
+
+            Console.WriteLine("Please enter the second number:");
+            y = int.Parse(Console.ReadLine());// explicit casting 
+
+
+            // processing
+
+            int Result = x + y;
+
+
+            // output 
+
+            Console.WriteLine("The result of adding {0} and {1} is {2}",x,y,Result);
+
+
+        }
+    }
+}
